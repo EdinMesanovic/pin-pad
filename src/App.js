4f05e1b944e6a71c7f4580fccc3924a0d7531ce1
EdoSlash999 <edo_tz_@hotmail.com>
2021-05-29 17:50:35 +0200

    pin-pad

---
+++ src/App.js
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+let counter = 0;
+function App() {
+  const [state, setState] = useState("");
+  const [disabled, setDisabled] = useState(false);
+
+  const clickHandle = (e) => {
+    setState(state + e.target.value);
+  };
+  const button = document.querySelectorAll(".disabled");
+  const enter = () => {
+    if (state === "") {
+      alert("Field is empty");
+    } else {
+      if (state === "1234") {
+        document.querySelector(".input-pin").setAttribute("type", "text");
+        setState("OK");
+        setDisabled(true);
+      } else {
+        document.querySelector(".input-pin").setAttribute("type", "text");
+        setState("ERROR");
+        setDisabled(true);
+        counter = counter + 1;
+
+        if (counter === 3) {
+          setState("LOCKED");
+          setDisabled(true);
+          document
+            .querySelector(".clear")
+            .setAttribute("disabled", "{disabled}");
+          setTimeout(() => {
+            document.querySelector(".clear").removeAttribute("disabled");
+            counter = 0;
+          }, 30000);
+        }
+      }
+    }
+  };
+
+  const clear = () => {
+    document.querySelector(".input-pin").setAttribute("type", "password");
+    setState("");
+    setDisabled(false);
+  };
+
+  return (
+    <div className='container'>
+      <input value={state} className='input-pin' type='password'></input>
+      <div className='grid-container'>
+        <button
+          value='1'
+          onClick={clickHandle}
+          className='grid-item'
+          disabled={disabled}
+        >
+          1
+        </button>
+        <button
+          value='2'
+          onClick={clickHandle}
+          className='grid-item'
+          disabled={disabled}
+        >
+          2
+        </button>
+        <button
+          value='3'
+          onClick={clickHandle}
+          className='grid-item'
+          disabled={disabled}
+        >
+          3
+        </button>
+        <button
+          value='4'
+          onClick={clickHandle}
+          className='grid-item'
+          disabled={disabled}
+        >
+          4
+        </button>
+        <button
+          value='5'
+          onClick={clickHandle}
+          className='grid-item '
+          disabled={disabled}
+        >
+          5
+        </button>
+        <button
+          value='6'
+          onClick={clickHandle}
+          className='grid-item'
+          disabled={disabled}
+        >
+          6
+        </button>
+        <button
+          value='7'
+          onClick={clickHandle}
+          className='grid-item'
+          disabled={disabled}
+        >
+          7
+        </button>
+        <button
+          value='8'
+          onClick={clickHandle}
+          className='grid-item'
+          disabled={disabled}
+        >
+          8
+        </button>
+        <button
+          value='9'
+          onClick={clickHandle}
+          className='grid-item'
+          disabled={disabled}
+        >
+          9
+        </button>
+        <button value='clear' onClick={clear} className='grid-item clear'>
+          clear
+        </button>
+        <button
+          value='0'
+          onClick={clickHandle}
+          className='grid-item'
+          disabled={disabled}
+        >
+          0
+        </button>
+        <button
+          value='enter'
+          onClick={enter}
+          className='grid-item'
+          disabled={disabled}
+        >
+          enter
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>PIN PAD PROJECT</h1>
+    </div>
+  );
+};
+
+export default Header;
